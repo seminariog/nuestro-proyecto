dc5fd7dc97b799219b95608c458178a22060c84c
miguel mamani mora <miguelmora392.mm@gmail.com>
2017-05-26 17:45:06 -0400

    corrigiendo login

--- client/templates/ingreso.js
+++ client/templates/ingreso.js
@@ -4,6 +4,8 @@ Template.ingreso.events({
 		var username = e.target.username.value;
 		var password = e.target.password.value;
 		console.log(username);
+		e.target.username.value="";
+		e.target.password.value="";
 		Meteor.loginWithPassword(username, password);
 		$('#ingresar').modal('hide');
 		FlowRouter.go('/dashboard');
