04a0b6e1bccba964a552681df11a3e532ea55306
miguel mamani mora <miguelmora392.mm@gmail.com>
2017-06-16 10:20:24 -0400

    2do sprint

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	/*---- Publicaciones ----*/
+	Meteor.publish('datosUsuario', function(){
+		return Meteor.users.find({_id: this.userId});
+	});
 });
